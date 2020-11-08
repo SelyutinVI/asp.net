5c79426d2fec5af1adfb7142ffb3043a54baace7
Владислав <azenshpis@gmail.com>
2020-11-08 20:40:53 +0500

    а

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -18,9 +18,26 @@ namespace WebApplication1.Pages
             obj = new MyObject();
         }
 
-        public void OnGet(MyObject obj)
+        public void OnPost(List<int> MyInt, List<string> MyString)
         {
-            obj = this.obj;
+            Type type = obj.GetType();
+            var props = type.GetProperties();
+            int i = 0;
+            foreach(var p in props)
+            {
+                if(Type.Equals(p.PropertyType,typeof(int)))
+                {
+                    p.SetValue(obj, MyInt[i]); i++;
+                }
+            }
+            i = 0;
+            foreach (var p in props)
+            {
+                if (Type.Equals(p.PropertyType, typeof(string)))
+                {
+                    p.SetValue(obj, MyString[i]); i++;
+                }
+            }
         }
     }
 }
