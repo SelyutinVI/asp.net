91039cd072778e65f33b89442225adc0b63511bc
Владислав <azenshpis@gmail.com>
2020-11-08 20:12:18 +0500

    Добавьте файлы проекта.

---
+++ Object/MyObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Object
+{
+    public class MyObject
+    {
+        public int _int { get; set; } = 1111;
+        public string _string { get; set; } = "Hello world!";
+        public int _int2 { get; set; } = 2222;
+        public double _double { get; set; } = 3333.333;
+        public bool _bool { get; set; } = true;
+
+        protected int _intProt { get; set; } = 444444;
+        private int _intPriv { get; set; } = 555;
+        public int _intFunc() {
+            return 666666;
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Object;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public MyObject obj;
+
+        public IndexModel()
+        {
+            obj = new MyObject();
+        }
+
+        public void OnGet(MyObject obj)
+        {
+            obj = this.obj;
+        }
+    }
+}
